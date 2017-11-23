b48fad8c7ab44dc15cbc67e6c1a25b35b89b16b7
Aykut Asil <aykuttasil@gmail.com>
2017-11-23 15:42:30 +0300

    githup api async-await

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,17 @@ import style from '../assets/stylesheets/styles.scss';
 const App = () => {
     console.log("Aykut Asil");
     console.log("Aykut Asillll");
+
+    getGithubUser('aykuttasil').then(user => {
+        console.log(user);
+        console.log(user.login);
+    }).catch(err => console.log(err));
+
 };
+
+async function getGithubUser(username) { // promise + await keyword usage allowed
+    const response = await fetch(`https://api.github.com/users/${username}`); // Execution stops here until fetch promise is fulfilled
+    return response.json();
+}
 
 export default App;
